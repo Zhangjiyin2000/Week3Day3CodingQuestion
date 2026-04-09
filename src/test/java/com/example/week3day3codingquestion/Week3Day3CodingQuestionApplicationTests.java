e78a04f9b7cf17599cb5cd5daf2cadea67cc0f34
Jiyin Zhang <zhangjiyin2000@gmail.com>
2026-04-08 23:54:44 -0300

    created a spring proj

---
+++ src/test/java/com/example/week3day3codingquestion/Week3Day3CodingQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week3day3codingquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3Day3CodingQuestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
